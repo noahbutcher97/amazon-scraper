55851ac1d3630fc1203c103ebc5c1363e8120db6
noahbutcher97 <58443849+noahbutcher97@users.noreply.github.com>
2021-02-01 22:32:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# LearnCode
+#This repository will be dedicated towards my efforts at learning to code for game development.
+If I make even a dollar out of this endeavour it will have all been worth it.
+It is the most fascinating nexus of science and entertainment media and I cannot reasonably fathom dedicating my time towards anything else. So lets learn some C#! Here Goes Nothing!
---
+++ amazon.py
@@ -0,0 +1,44 @@
+from selectorlib import Extractor
+import requests
+import json
+from time import sleep
+
+e = Extractor.from_yaml_file('selectors.yaml')
+#'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+
+def scrape(url):
+    headers = {
+        'authority':'www.amazon.com',
+        'pragma':'no-cache',
+        'cache-control':'no-cache',
+        'dnt':'1',
+        'upgrade-insecure-requests':'1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site':'none',
+        'sec-fetch-mode':'navigate',
+        'sec-fetch-dest':'document',
+        'accept-language':'en-GB,en-US;q=0.9,en;q=0.8',
+        }
+    print("Downloading %s"%url)
+    r = requests.get(url, headers=headers)
+    #Check if page was blocked (usually 503)
+    if r.status_code > 500:
+        if "To discuss automated access to Amazon data please contact" in r.text:
+            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+        else:
+            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+        return None
+    #Pass the HTML of the page and create
+    return e.extract(r.text)
+# product_data = []
+with open("urls.txt",'r') as urllist, open('output.json','w') as outfile:
+    for url in urllist.readlines():
+        data = scrape(url)
+        if data:
+            json.dump(data,outfile)
+            outfile.write("\n")
+            
+        
+        
+
